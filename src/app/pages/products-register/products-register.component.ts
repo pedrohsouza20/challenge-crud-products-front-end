3663c86059d2a90f920932d0b007c4f6f640e4f3
Pedro Souza <contato.pedromoc@gmail.com>
2025-02-01 16:03:54 -0300

    Create and configure the page of register of products

--- src/app/pages/products-register/products-register.component.ts
+++ src/app/pages/products-register/products-register.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-products-register',
   imports: [],
   templateUrl: './products-register.component.html',
-  styleUrl: './products-register.component.scss'
+  styleUrl: './products-register.component.scss',
 })
 export class ProductsRegisterComponent {
-
+  constructor(private router: Router) {}
+  public goToList(): void {
+    this.router.navigate(['/products-list']);
+  }
 }
